5fb52578661a607c96a4362879a4c0b28e5fb621
t.hirai <t_hirai@avelio.co.jp>
2023-11-15 18:01:27 +0900

    TypeORM設定追加

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,7 +1,26 @@
 import { Module } from '@nestjs/common';
 import { TasksModule } from './tasks/tasks.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmConfigService } from './config/typeorm-config.service';
 
 @Module({
-  imports: [TasksModule],
+  imports: [
+    /**
+     * env読込
+     */
+    ConfigModule.forRoot({
+      envFilePath: [`.env/${process.env.NODE_ENV}.env`, '.env/default.env'],
+      isGlobal: true,
+    }),
+    /**
+     * TypeORMの設定を非同期取得に変更
+     */
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useClass: TypeOrmConfigService,
+    }),
+    TasksModule,
+  ],
 })
 export class AppModule {}
