149459683ea22b6012bccc235335dc92def0de13
t.hirai <t_hirai@avelio.co.jp>
2023-11-15 19:11:07 +0900

    service修正、テスト追加

--- backend/src/tasks/tasks.controller.ts
+++ backend/src/tasks/tasks.controller.ts
@@ -12,21 +12,24 @@ import {
 } from '@nestjs/common';
 import { TaskPropertyDto } from './dto/task-property.dto';
 import { TaskStatusPipe } from './pipe/task-status.pipe';
+import { TasksService } from './tasks.service';
+import { Task } from './task.entity';
 
 @Controller('tasks')
 export class TasksController {
+  constructor(private tasksService: TasksService) {}
   // @Getとか@PostとかはHTTPリクエストのデコレータ
   @Get()
-  getTasks(): string {
-    return 'getTasks Success!';
+  getTasks(): Promise<Task[]> {
+    return this.tasksService.getTasks();
   }
 
   // @Paramはリクエストパラメータのデコレータ
   // ParseIntPipeはメソッドに値が引き渡される前に実行され、変換or検証を行う
   // 今回は'id'の変換
   @Get('/:id')
-  getTaskById(@Param('id', ParseIntPipe) id: number): string {
-    return `getTaskById Success! Parameter [id:${id}]`;
+  getTaskById(@Param('id', ParseIntPipe) id: number): Promise<Task> {
+    return this.tasksService.getTaskById(id);
   }
 
   @Post()
@@ -35,21 +38,20 @@ export class TasksController {
   createTask(
     // @Bodyはリクエストボディのパラメータ
     @Body() taskPropertyDto: TaskPropertyDto,
-  ): string {
-    const { title, description } = taskPropertyDto;
-    return `createTask Success! Parameter [title:${title}, description:${description}]`;
+  ): Promise<Task> {
+    return this.tasksService.createTask(taskPropertyDto);
   }
 
   @Delete('/:id')
-  deleteTask(@Param('id', ParseIntPipe) id: number): string {
-    return `deleteTask Success! Parameter [id:${id}]`;
+  deleteTask(@Param('id', ParseIntPipe) id: number): Promise<void> {
+    return this.tasksService.deleteTask(id);
   }
 
   @Patch('/:id')
   updateTask(
     @Param('id', ParseIntPipe) id: number,
     @Body('status', TaskStatusPipe) status: string,
-  ): string {
-    return `updateTask Success! Parameter [id:${id}, status:${status}]`;
+  ): Promise<Task> {
+    return this.tasksService.updateTask(id, status);
   }
 }
